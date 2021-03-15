1e64662edeb1e30fcb7bcce91cac666883db7860
kimminji000 <minji6764@naver.com>
2021-03-15 13:51:19 +0900

    Modify management targets from students to books

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,6 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("1. Add Books");
+		System.out.println("2. Delete Books");
+		System.out.println("3. Edit Books");
+		System.out.println("4. View Books");
+		System.out.println("5. Exit");
 	}
 }
