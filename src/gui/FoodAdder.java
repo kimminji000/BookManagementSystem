6d6a18673dabeb6329d91f85a5116ed8b83ef5f1
kimminji000 <minji6764@naver.com>
2021-06-01 05:24:33 +0900

    add gui

---
+++ src/gui/FoodAdder.java
@@ -0,0 +1,54 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class FoodAdder extends JFrame {
+	
+	public FoodAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelID = new JLabel("ID: ", JLabel.TRAILING);
+		JTextField fieldID = new JTextField(10);
+		labelID.setLabelFor(fieldID);
+		panel.add(labelID);
+		panel.add(fieldID);
+		
+		JLabel labelName = new JLabel("Name: ", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelName.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		JLabel labelType = new JLabel("Type: ", JLabel.TRAILING);
+		JTextField fieldType = new JTextField(10);
+		labelType.setLabelFor(fieldType);
+		panel.add(labelType);
+		panel.add(fieldType);
+		
+		JLabel labelExpirationdate = new JLabel("Expirationdate: ", JLabel.TRAILING);
+		JTextField fieldExpirationdate = new JTextField(10);
+		labelExpirationdate.setLabelFor(fieldType);
+		panel.add(labelExpirationdate);
+		panel.add(fieldExpirationdate);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("canel"));
+		
+		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6);
+		
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setContentPane(panel);
+		this.setVisible(true);
+		
+		
+	}
+
+}
---
+++ src/gui/GuiTest.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class GuiTest {
+	
+	public static void main(String[] args) {
+		//MenuSelection menuselection = new MenuSelection();
+		//FoodAdder foodadder = new FoodAdder();
+		FoodViewer foodviewer = new FoodViewer();
+	}
+
+}
